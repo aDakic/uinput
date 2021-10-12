824f9dd0c1e34503a484d7fbe2cb836c7d8084a6
Aleksandar Dakic <aleksandardakic16@gmail.com>
2021-10-12 05:41:44 -0700

    clang formating

--- src/system/file_descriptor.cpp
+++ src/system/file_descriptor.cpp
@@ -4,62 +4,61 @@
 
 namespace fd
 {
-
-file_descriptor::file_descriptor(const file_descriptor& other)
-{
-    log_info(m_file_descriptor_tag, "Copy constructor\n");
-    *this = other;
-}
-
-file_descriptor& file_descriptor::operator=(const file_descriptor& other)
-{
-    log_info(m_file_descriptor_tag, "Copy {}\n", __FUNCTION__);
-
-    if (this != &other)
+    file_descriptor::file_descriptor(const file_descriptor& other)
     {
-        int dup_fd = dup(other.m_fd);
-        if (-1 == dup_fd)
+        log_info(m_file_descriptor_tag, "Copy constructor\n");
+        *this = other;
+    }
+
+    file_descriptor& file_descriptor::operator=(const file_descriptor& other)
+    {
+        log_info(m_file_descriptor_tag, "Copy {}\n", __FUNCTION__);
+
+        if (this != &other)
         {
-            throw std::runtime_error{"Failed to crate file descriptor duplicate.\n"};
+            int dup_fd = dup(other.m_fd);
+            if (-1 == dup_fd)
+            {
+                throw std::runtime_error{ "Failed to crate file descriptor duplicate.\n" };
+            }
+
+            ::close(m_fd);
+            m_fd = dup_fd;
         }
 
-        ::close(m_fd);
-        m_fd = dup_fd;
+        return *this;
     }
 
-    return *this;
-}
-
-file_descriptor::file_descriptor(file_descriptor&& other) noexcept
-{
-    log_info(m_file_descriptor_tag, "Move constructor\n");
-    *this = std::move(other);
-}
-
-file_descriptor& file_descriptor::operator=(file_descriptor&& other) noexcept
-{
-    log_info(m_file_descriptor_tag, "Move {}\n", __FUNCTION__);
-
-    if (this != &other)
+    file_descriptor::file_descriptor(file_descriptor&& other) noexcept
     {
-        m_fd = std::exchange(other.m_fd, -1);
+        log_info(m_file_descriptor_tag, "Move constructor\n");
+        *this = std::move(other);
     }
 
-    return *this;
-}
-
-file_descriptor::~file_descriptor() noexcept
-{
-    log_info(m_file_descriptor_tag, "{}\n", __FUNCTION__);
-
-    if (-1 != m_fd)
+    file_descriptor& file_descriptor::operator=(file_descriptor&& other) noexcept
     {
-        if (-1 == ::close(m_fd))
+        log_info(m_file_descriptor_tag, "Move {}\n", __FUNCTION__);
+
+        if (this != &other)
         {
-            log_warning(m_file_descriptor_tag, "The file descriptor is not closed properly.\n");
+            m_fd = std::exchange(other.m_fd, -1);
+        }
+
+        return *this;
+    }
+
+    file_descriptor::~file_descriptor() noexcept
+    {
+        log_info(m_file_descriptor_tag, "{}\n", __FUNCTION__);
+
+        if (-1 != m_fd)
+        {
+            if (-1 == ::close(m_fd))
+            {
+                log_warning(m_file_descriptor_tag, "The file descriptor is not closed properly.\n");
+            }
         }
     }
-}
 
-file_descriptor::operator bool() const noexcept { return -1 != m_fd; }
-}
+    file_descriptor::operator bool() const noexcept { return -1 != m_fd; }
+}  // namespace fd
